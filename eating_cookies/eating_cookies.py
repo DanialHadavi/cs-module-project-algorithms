2858d38c2423f4943f86e9f1cdd300289d05e7ca
Danial Hadavi <danialhadavi@gmail.com>
2020-06-17 19:18:19 -0400

    fixed cookies

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -6,17 +6,18 @@ Returns: an integer
 
 def eating_cookies(n, cache=None):
     # Your code here
-    if cache == None:
-        cache = [0] * (n + 1)
-    if n <= 1:
-        cache[n] = 1
-    elif n == 2:
-        cache[n] = 2
-    elif cache[n] == 0:
+    if n == 0:
+        return 1
+    elif n < 0:
+        return 0
+    elif cache and cache[n] > 0:
+        return cache[n]
+    else:
+        if not cache:
+            cache = {i: 0 for i in range(n+1)}
         cache[n] = eating_cookies(
-            n - 1, cache) + eating_cookies(n - 2, cache) + eating_cookies(n - 3, cache)
-
-    return cache[n]
+            n-1, cache) + eating_cookies(n-2, cache) + eating_cookies(n-3, cache)  # this is where the magic happens
+        return cache[n]
 
 
 if __name__ == "__main__":
@@ -24,4 +25,4 @@ if __name__ == "__main__":
     num_cookies = 5
 
     print(
-        f"There are {eating_cookies(num_cookies)} ways for Cookie Monster to each {num_cookies} cookies")
+        f"There are {eating_cookies(num_cookies)} ways for Cookie Monster to eat {num_cookies} cookies")
